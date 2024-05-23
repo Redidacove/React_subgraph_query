4bbb45385727cb92430423ff39c3d51614592bb9
Md Amaan <Redidacove>
2024-05-23 23:40:14 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache,
+    useQuery,
+} from "@apollo/client";
+import { gql } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://gateway-arbitrum.network.thegraph.com/api/[<your-api-key>]/subgraphs/id/4TbqVA8p2DoBd5qDbPMwmDZv3CsJjWtxo8nVSqF2tA9a",
+
+    cache: new InMemoryCache(),
+});
+
+const QUERY = gql`
+	query MyQuery {
+        tokens(first: 5) {
+          id
+          name
+          symbol
+          decimals
+        }
+        rewardTokens(first: 5) {
+          id
+          token {
+            id
+          }
+          type
+        }
+      }
+`;
+
+function MyComponent() {
+    const { data, error, loading } = useQuery(QUERY);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
+    console.log(JSON.stringify(data, null, 2));
+    return (
+        <div>
+            <p>Data fetched successfully</p>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <MyComponent />
+        </ApolloProvider>
+    );
 }
 
 export default App;
